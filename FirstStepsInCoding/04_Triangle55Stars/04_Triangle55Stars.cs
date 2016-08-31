44c38d0e2bd48af97f58eb40c07bcc1c054373f2
Станимир Станков <stankov65@gmail.com>
2016-08-31 08:32:03 +0300

    add example
    Drawing a triangle of stars.

---
+++ FirstStepsInCoding/04_Triangle55Stars/04_Triangle55Stars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int row = 1; row <= 10; row++)
+        {
+            string asterisk = new string('*', row);
+            Console.WriteLine(asterisk);
+        }
+    }
+}
+
