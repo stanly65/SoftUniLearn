e2b08890f3be49d371e13f56678f7a14b6bc0d00
Станимир Станков <stankov65@gmail.com>
2016-09-01 16:33:20 +0300

    change structure
    change structure

---
+++ ProgrammingBasics/SimpleCalculations/01_SquareArea/01_SquareArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int site = int.Parse(Console.ReadLine());
+        int surface = site * site;
+        Console.WriteLine(surface);
+    }
+}
+
---
+++ ProgrammingBasics/SimpleCalculations/02_InchesToCentimeters/02_InchesToCentimeters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var num = decimal.Parse(Console.ReadLine());
+        var inch = 2.54m;
+
+        var cm = num * inch;
+        Console.WriteLine(cm);
+    }
+}
+
