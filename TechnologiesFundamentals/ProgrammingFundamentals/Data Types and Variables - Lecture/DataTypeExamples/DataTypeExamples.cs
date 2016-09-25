e67a192ec684cea2d132ba6073fa2737b93a7a3d
Stanimir Stankov <stankov65@gmail.com>
2016-09-26 00:13:19 +0300

    add

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Data Types and Variables - Lecture/DataTypeExamples/DataTypeExamples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        byte counter = 0; // [0...255]
+        for (int i = 0; i < 260; i++)
+        {
+            counter++;
+            Console.WriteLine(counter);
+        
+        }
+    }
+}
+
